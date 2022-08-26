e1ec71b9314304631cf364dae4f4dc049eddb3d1
Aigul Mustafina <aimustafina2016@gmail.com>
2022-08-26 11:31:56 +0600

    Added Page Object for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,20 @@
 from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_should_be_login_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
